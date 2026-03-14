5e44a91edd91d2884f393431c6e0491f6a157cb9
prostovelikij <oobobb@yandex.kz>
2026-03-14 14:39:07 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -7,7 +7,7 @@ sed -i 's|^#SRC_URL=https://github.com/aarond10|SRC_URL=https://github.com/aaron
 rm -f padavan-ng/trunk/user/doh_proxy/https_dns_proxy-master.tar.gz
 
 # 3. Переходим в папку и удаляем всё лишнее, кроме Makefile и патчей
-cd padavan-ng/trunk/user/doh_proxy
+cd padavan-ng/trunk/user/nfqws/Makefile
 find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
 
 
